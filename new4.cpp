d078c81426dd50bba8d108cd9c3dde9530a08481
rishabhvarshney2 <35600120+rishabhvarshney2@users.noreply.github.com>
2019-02-18 14:36:29 +0530

    Add files via upload

---
+++ new4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,k,sum=0;
+	cin>>n>>k;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<k;i++){
+		sum=sum+arr[i];
+	}
+	cout<<sum;
+	return 0;
+	
+	
+}
